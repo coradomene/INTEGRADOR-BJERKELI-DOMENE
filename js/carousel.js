781bc698cac04ec19b7744e17714746625b1a5cb
DOMENE <coradomene@gmail.com>
2025-10-10 15:11:29 -0300

    carrousel funcionando, falta agranadar botones

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded",function(){ const images = [
+document.addEventListener("DOMContentLoaded",function(){ const imagenes = [
     "img/gatito1.jpeg",
     "img/gatito2.jpeg",
     "img/gatito3.jpeg",
@@ -12,24 +12,29 @@ const imgE1 = document.getElementById("imagen");
 const btnNext = document.getElementById("next");
 const btnPrev = document.getElementById("prev");
 
-if (!imgE1|| !btnNext  || !btnPrev) return;
 
 function mostrarImagen(){ 
     imgE1.src = imagenes[indice];
+        console.log("mostrando: ",imagenes [indice]);
     }
     
-    function siguiente(){
-        indice = (indice+1)% imagenes.length;
-        mostrarImagen();
-    }
 
-    function anterior(){
-        indice = (indice-1+ imagenes.length)% imagenes.length;
+    btnNext.addEventListener("click",()=>{
+        indice++;
+        if(indice >= imagenes.length){
+            indice=0;
+        }
         mostrarImagen();
-    }
+    });
 
-    btnNext.addEventListener("click",siguiente);
-    btnPrev.addEventListener("click",anterior);
+    btnPrev.addEventListener("click",()=>{
+        indice--;
+        if(indice<0){
+            indice = imagenes.length -1;
+        }
+    
+    mostrarImagen();
+});
     mostrarImagen();
 });
 
