c2fb6ee2e5f0a6c0f000c86bdf5206424ff02737
DOMENE <coradomene@gmail.com>
2025-10-13 20:48:35 -0300

    mejoras en css, js y carrusel

--- js/carousel.js
+++ js/carousel.js
@@ -7,15 +7,26 @@ document.addEventListener("DOMContentLoaded",function(){ const imagenes = [
     "img/gatito6.jpeg",
     "img/gatito7.jpeg",
 ]
+const textos = [
+    "Brayan: le encanta jugar, comer y dormir, ya que es pequeño aun ",
+    "Coco: es muy timido pero muy amistoso",
+    "Kevin: es muy travieso pero muy cariñoso",
+    "Jaimito: rescatado de la calle, por eso su condicion, esta hace 6 meses con nosotros es super cariñoso",
+    "Ina: ella llego sola al refugio es mimosa pero no le gustan las fotos jaja",
+    "Gaston: rescatado de la calle, es un gato grande que solo busca tranquilidad y amor",
+    "Caramelo: llego hace tres meses y ama tomar leche (deslactosada)",
+]
 let indice =0;
+
 const imgE1 = document.getElementById("imagen");
+const textoE1 = document.getElementById("texto-imagen");
 const btnNext = document.getElementById("next");
 const btnPrev = document.getElementById("prev");
 
 
 function mostrarImagen(){ 
     imgE1.src = imagenes[indice];
-        console.log("mostrando: ",imagenes [indice]);
+    textoE1.textContent = textos[indice];
     }
     
 
