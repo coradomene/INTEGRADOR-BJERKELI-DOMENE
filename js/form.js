3c8e9093d5dd87390bb5e0f08560efe6749316f1
Kjellfrid Bjerkeli <kjellfrid.18@gmail.com>
2025-10-25 22:36:29 -0300

    Continuacion del js form

--- js/form.js
+++ js/form.js
@@ -13,5 +13,37 @@ document.addEventListener("DOMContentLoaded" , function()) {
 
         const regexEmail = /^[\w.-]+@[a-zA-Z09.-]+\.[a-zA-Z]{2,}$/;
         
+        let errores = [];
+
+        if (nombre === "");
+        errores.push("Por favor ingresa tu nombre.");
     }
-}
+
+        if (!regexEmail.test(email)) { 
+            errores.push("Ingresa un Correo Electronico valido.");
+        }
+
+        if (!regexTelefono.test(telefono)) {
+        errores.push("El teléfono debe tener entre 7 y 15 números.");
+    }  
+
+        if (errores.length > 0) {
+        errores.forEach(error => {
+        const p = document.createElement("p");
+        p.style.color = "red";
+        p.textContent = error;
+        resultado.appendChild(p);
+    });
+
+    } else {
+        const mensaje = document.createElement("p");
+        mensaje.style.color = "green";
+        mensaje.textContent = `Gracias, ${nombre}! Tus datos fueron enviados correctamente.`;
+        resultado.appendChild(mensaje);
+
+            form.reset();
+        }
+    });
+});
+
+
