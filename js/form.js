ca11720bdd46824396a295dc99d74e2820bd7a9e
DOMENE <coradomene@gmail.com>
2025-10-27 20:10:16 -0300

    errores de tipeo

--- js/form.js
+++ js/form.js
@@ -1,14 +1,17 @@
+console.log("form.js se esta ejecutando correctamente");
 document.addEventListener("DOMContentLoaded" , function() {
     const form = document.getElementById("formContacto");
     const resultado = document.getElementById("resultado");
 
     form.addEventListener("submit", function (event){
         event.preventDefault(); 
+        console.log(" Se detectó el envío del formulario");
+
         resultado.innerHTML = "";
 
         const nombre = document.getElementById("nombre").value.trim();
         const email = document.getElementById("email").value.trim();
-        const telefono = document.getElementById("telefono").value.trim();
+        const telefono = document.getElementById("telefono").value.trim ();
 
 
         const regexEmail = /^[\w.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -32,7 +35,11 @@ document.addEventListener("DOMContentLoaded" , function() {
         if (errores.length > 0) {
         errores.forEach(error => {
         const p = document.createElement("p");
-        p.style.color = "red";
+        p.style.color = "black";
+        p.style.backgroundColor = "red";
+        p.style.padding = "8px";
+        p.style.borderRadius = "6px";
+        p.style.marginBottom = "8px";
         p.textContent = error;
         resultado.appendChild(p);
     });
