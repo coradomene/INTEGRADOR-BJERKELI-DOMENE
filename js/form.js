2e30c97e611b7d3a4404c83f19ef27fdab193ece
Kjellfrid Bjerkeli <kjellfrid.18@gmail.com>
2025-11-01 15:22:35 -0300

    Arreglos en form js y contacto

--- js/form.js
+++ js/form.js
@@ -17,31 +17,34 @@ document.addEventListener("DOMContentLoaded" , function() {
         const email = document.getElementById("email").value.trim();
         const telefono = document.getElementById("telefono").value.trim();
 
-        const regexNombre = /^[a-zA-Z]{2,20}$/;
+        const regexNombre = /^[a-zA-Z]{3,20}$/;
         const regexEmail = /^[\w.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         const regexTelefono = /^[0-9]{7,15}$/;   
         
+        document.getElementById("telefono").addEventListener("input", function() {
+        this.value = this.value.replace(/[^0-9]/g, ""); 
+});
         
         let hayError= false;
 
         if (nombre === ""){
-            document.getElementById("errorNombre").textContent = "por favor ingrese su nombre";
+            document.getElementById("errorNombre").textContent = "Por favor ingrese su nombre";
         hayError = true
     }   else if (!regexNombre.test(nombre)) {
-            document.getElementById("errorNombre").textContent = "Ingresa un nombre con minimo 2 y maximo 20 caracteres validos.";
+            document.getElementById("errorNombre").textContent = "Ingresa un nombre con minimo 3 y maximo 20 caracteres validos.";
             hayError = true;
 
-        if (email==="") {document.getElementById("errorEmail").textContent = "por favor ingresa tu email";
+        if (email==="") {document.getElementById("errorEmail").textContent = "Por favor ingresa tu email";
             hayError = true
         }else if (!regexEmail.test(email)) {
             document.getElementById("errorEmail").textContent = "Ingresa un Correo Electronico valido.";
             hayError = true;
         }
         if (telefono===""){ 
-            document.getElementById("errorTelefono").textContent = "por favor ingrese su telefono";
+            document.getElementById("errorTelefono").textContent = "Por favor ingrese su telefono";
             hayError = true;
         }else if (!regexTelefono.test(telefono)){
-        document.getElementById("errorTelefono").textContent = "el telefono debe tener entre 7 y 15 numeros";
+        document.getElementById("errorTelefono").textContent = "El telefono debe tener entre 7 y 15 numeros";
         hayError = true;
     }  
 
@@ -49,16 +52,9 @@ document.addEventListener("DOMContentLoaded" , function() {
         mensajeFinal.textContent = `Gracias, ${nombre}! Tus datos fueron enviados con éxito.`;
         mensajeFinal.style.color = "green";
 
-        //form.reset();
-        
-        try {
         form.reset();
-    } catch (e) {
-    console.warn("form.reset() falló:", e);
-    }
         
         }
-        
         }  
     });
 });
